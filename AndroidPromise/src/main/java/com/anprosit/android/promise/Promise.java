e4197b7b21a3a800fb914f685ce21b27c6a043f7
Hirofumi Nakagawa <nklabs@gmail.com>
2013-10-06 00:19:00 +0900

    Modify Promise class

--- AndroidPromise/src/main/java/com/anprosit/android/promise/Promise.java
+++ AndroidPromise/src/main/java/com/anprosit/android/promise/Promise.java
@@ -1,15 +1,12 @@
 package com.anprosit.android.promise;
 
-import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 
 import com.anprosit.android.promise.internal.PromiseImpl;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.WeakHashMap;
@@ -18,7 +15,7 @@ import java.util.WeakHashMap;
  * Created by Hirofumi Nakagawa on 13/07/12.
  */
 public abstract class Promise<I, O> {
-	private static Map<Context, Set<Promise<?, ?>>> mPromises = new WeakHashMap<Context, Set<Promise<?, ?>>>();
+	private static Map<Object, Set<Promise<?, ?>>> mPromises = new WeakHashMap<Object, Set<Promise<?, ?>>>();
 
 	public abstract <NO> Promise<I, NO> then(Task<O, NO> task);
 
@@ -46,12 +43,12 @@ public abstract class Promise<I, O> {
 
 	public abstract boolean isCancelled();
 
-	public static synchronized <T> Promise<T, T> newInstance(Context context, Class<T> in) {
-		return newInstance(context, in, new Handler(Looper.getMainLooper()));
+	public static synchronized <T> Promise<T, T> newInstance(Object lifecycle, Class<T> in) {
+		return newInstance(lifecycle, in, new Handler(Looper.getMainLooper()));
 	}
 
-	public static synchronized <T> Promise<T, T> newInstance(Context context, Class<T> in, Handler handler) {
-		Set<Promise<?, ?>> set = mPromises.get(context);
+	public static synchronized <T> Promise<T, T> newInstance(Object lifecycle, Class<T> in, Handler handler) {
+		Set<Promise<?, ?>> set = mPromises.get(lifecycle);
 		if (set == null)
 			set = new HashSet<Promise<?, ?>>();
 
@@ -60,9 +57,9 @@ public abstract class Promise<I, O> {
 		return instance;
 	}
 
-    public static synchronized void destroy(Context context, Promise<?, ?> promise) {
+    public static synchronized void destroy(Object lifecycle, Promise<?, ?> promise) {
         try {
-            Set<Promise<?, ?>> set = mPromises.remove(context);
+            Set<Promise<?, ?>> set = mPromises.remove(lifecycle);
             if (set == null)
                 return;
             set.remove(promise);
@@ -71,8 +68,8 @@ public abstract class Promise<I, O> {
         }
     }
 
-	public static synchronized void destroy(Context context) {
-		Set<Promise<?, ?>> set = mPromises.remove(context);
+	public static synchronized void destroy(Object lifecycle) {
+		Set<Promise<?, ?>> set = mPromises.remove(lifecycle);
 		if (set == null)
 			return;
 
